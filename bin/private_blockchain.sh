53b452c91ed02c138307f6f47c6c03f4dc79c2b3
smvv21 <zxw12002@gmail.com>
2016-07-26 16:19:56 +0800

    Update private_blockchain.sh

--- bin/private_blockchain.sh
+++ bin/private_blockchain.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 geth=${GETH:-geth}
-
-$geth --datadir data --genesis ./genesis.json --networkid 31415926 --rpc --rpccorsdomain "*" console
+# it must establish from the same genesis.json if they are brother nodes!
+$geth --datadir data init ./genesis.json
+# default lenstening  localhost:8545  and  30303 p2p port
+$geth --datadir data --networkid 162161 --rpc --rpccorsdomain "*" console
